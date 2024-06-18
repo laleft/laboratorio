c771c332d4d1bc962d50e9c2eebb30663abcd8f2
Santiago De Bernardez <sdebernardez@gmail.com>
2024-06-18 12:37:26 -0300

    chore: Refactor Computer class to use Location object instead of String

--- src/models/Computer.java
+++ src/models/Computer.java
@@ -10,8 +10,6 @@ import adt.AbstractNode;
 * @since   2024-06-14
 */
 public class Computer extends AbstractNode {
-    private boolean status; // true: activo, false: inactivo
-    private String location;
 
     /**
      * Crea una nueva instancia de la clase Computer.
@@ -22,44 +20,8 @@ public class Computer extends AbstractNode {
      * @param status el estado de la computadora (true: activo, false: inactivo)
      * @param location la ubicación de la computadora
      */
-    public Computer(String id, String ipAddress, String macAddress, boolean status, String location) {
-        super(id, ipAddress, macAddress, "Computer");
-        this.status = status;
-        this.location = location;
-    }
-
-    
-    
-    /**
-     * Devuelve el estado de la computadora.
-     * @return el estado de la computadora
-     */
-    public boolean isStatus() {
-        return status;
-    }
-
-    /**
-     * Establece el estado de la computadora.
-     * @param status el estado de la computadora
-     */
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    /**
-     * Devuelve la ubicación de la computadora.
-     * @return la ubicación de la computadora
-     */
-    public String getLocation() {
-        return location;
-    }
-
-    /**
-     * Establece la ubicación de la computadora.
-     * @param location la ubicación de la computadora
-     */
-    public void setLocation(String location) {
-        this.location = location;
+    public Computer(String id, String ipAddress, String macAddress, boolean status, Location location) {
+        super(id, ipAddress, macAddress, status, location);
     }
 
     /**
@@ -69,7 +31,7 @@ public class Computer extends AbstractNode {
     @Override
     public String toString() {
         return "Computer{id='" + getId() + "', ipAddress='" + getIpAddress() + "', macAddress='" + getMacAddress() + 
-               "', status=" + (status ? "activo" : "inactivo") + ", location='" + location + "'}";
+               "', status=" + (getStatus() ? "activo" : "inactivo") + ", location='" + getLocation() + "'}";
     }
 }
 
