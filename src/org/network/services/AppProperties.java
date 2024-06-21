3d29349f0f2b6e092211930dd5158e0007df07a9
Santiago De Bernardez <sdebernardez@gmail.com>
2024-06-21 15:40:52 -0300

    chore: Refactor AppProperties class to load properties from file

---
+++ src/org/network/services/AppProperties.java
@@ -0,0 +1,57 @@
+package org.network.services;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.HashMap;
+
+/**
+ * Clase que carga las propiedades de la aplicación.
+ * @version 1.0
+ * @since 2024-06-14
+ * @autor sdebernardez
+ */
+public class AppProperties {
+
+    private static final HashMap<String, String> properties = new HashMap<>();
+    
+    /**
+     * Crea una nueva instancia de la clase AppProperties.
+     * @param filename
+     */
+    public AppProperties(String filename) {
+        
+        Properties props = new Properties();
+
+        String appPropertiesPath = AppConstants.CONFIG_PATH + filename;
+        try {
+            props.load(new FileInputStream(new File(appPropertiesPath)));
+            for (String key : props.stringPropertyNames()) {
+                properties.put(key, props.getProperty(key));
+            }
+        } catch (FileNotFoundException fileerror) {
+            fileerror.printStackTrace();
+        } catch (IOException ioerror) {
+            ioerror.printStackTrace();
+        }
+    }
+
+    /**
+     * Devuelve las propiedades de la aplicación.
+     * @return las propiedades de la aplicación
+     */
+    public HashMap<String, String> getProperties() {
+        return properties;
+    }
+
+    /**
+     * Devuelve la propiedad con la clave especificada.
+     * @param key la clave de la propiedad
+     * @return la propiedad con la clave especificada
+     */
+    public String getProperty(String key) {
+        return properties.get(key);
+    }
+}
