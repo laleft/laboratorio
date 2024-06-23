08d639e5de30e3e548eaee1b978aeb9567805d77
Santiago De Bernardez <sdebernardez@gmail.com>
2024-06-23 18:07:36 -0300

    feat: Update Connection and Location classes with Javadoc comments and toString method

--- src/org/network/models/Connection.java
+++ src/org/network/models/Connection.java
@@ -2,6 +2,13 @@ package org.network.models;
 
 import org.network.interfaces.Device;
 
+/**
+ * Clase que representa una conexión.
+ * @version 1.0
+ * @since 2024-06-14
+ * @author sdebernardez
+ * @see Device
+ */
 public class Connection {
     
     private String id;
@@ -12,6 +19,16 @@ public class Connection {
     private int latency;
     private Double errorRate;
 
+    /**
+     * Crea una nueva instancia de la clase Connection.
+     * @param id el identificador de la conexión
+     * @param source el dispositivo origen
+     * @param target el dispositivo destino
+     * @param type el tipo de conexión
+     * @param bandwidth el ancho de banda de la conexión
+     * @param latency la latencia de la conexión
+     * @param errorRate la tasa de error de la conexión
+     */
     public Connection(String id, Device source, Device target, String type, int bandwidth, int latency, Double errorRate) {
         this.id = id;
         this.source = source;
@@ -22,35 +39,92 @@ public class Connection {
         this.errorRate = errorRate;
     }
 
+    /**
+     * Devuelve el identificador de la conexión.
+     * @return String
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Devuelve el dispositivo origen.
+     * @return Device
+     */
     public Device getSource() {
         return source;
     }
 
+    /**
+     * Devuelve el dispositivo destino.
+     * @return Device
+     */
     public Device getTarget() {
         return target;
     }
 
+    /**
+     * Devuelve el tipo de conexión.
+     * @return String
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Devuelve el ancho de banda de la conexión.
+     * @return int
+     */
     public int getBandwidth() {
         return bandwidth;
     }
 
+    /**
+     * Devuelve la latencia de la conexión.
+     * @return int
+     */
     public int getLatency() {
         return latency;
     }
 
+    /**
+     * Devuelve la tasa de error de la conexión.
+     * @return Double
+     */
     public Double getErrorRate() {
         return errorRate;
     }
 
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Connection other = (Connection) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Connection [id=" + id + ", source=" + source + ", target=" + target + ", type=" + type + ", bandwidth="
+                + bandwidth + ", latency=" + latency + ", errorRate=" + errorRate + "]";
+    }
 
 }
 
--- src/org/network/models/Location.java
+++ src/org/network/models/Location.java
@@ -1,11 +1,24 @@
 package org.network.models;
 
+/**
+ * Clase que representa una ubicación física.
+ * @version 1.0
+ * @since 2024-06-14
+ * @author sdebernardez
+ */
 public class Location {
     private String id;
     private String building;
     private String floor;
     private String room;
 
+    /**
+     * Crea una nueva instancia de la clase Location.
+     * @param id el identificador de la ubicación
+     * @param building el edificio
+     * @param floor el piso
+     * @param room la sala
+     */
     public Location(String id, String building, String floor, String room) {
         this.id = id;
         this.building = building;
@@ -13,30 +26,58 @@ public class Location {
         this.room = room;
     }
 
+    /**
+     * Devuelve el identificador de la ubicación.
+     * @return String
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Devuelve el edificio.
+     * @return String
+     */
     public String getBuilding() {
         return building;
     }
 
+    /**
+     * Establece el edificio.
+     * @param building nombre del edificio
+     */
     public void setBuilding(String building) {
         this.building = building;
     }
 
+    /**
+     * Devuelve el piso.
+     * @return String
+     */
     public String getFloor() {
         return floor;
     }
 
+    /**
+     * Establece el piso.
+     * @param floor el nombre o número de piso
+     */
     public void setFloor(String floor) {
         this.floor = floor;
     }
 
+    /**
+     * Devuelve la sala.
+     * @return String
+     */
     public String getRoom() {
         return room;
     }
 
+    /**
+     * Establece la sala.
+     * @param room el nombre o número de sala
+     */
     public void setRoom(String room) {
         this.room = room;
     }
@@ -45,4 +86,31 @@ public class Location {
     public String toString() {
         return "Location [id=" + id + ", building=" + building + ", floor=" + floor + ", room=" + room + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Location other = (Location) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 }
